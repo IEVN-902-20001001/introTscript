4de8dbd568bc3a18e53c0143ffd616612dd2f778
Miguel Hernandez <20001001@alumnos.utleon.edu.mx>
2023-09-09 17:30:42 -0500

    09/09/2023 trabajo clase

---
+++ introTscipt/src/ejemplos-ts/01-tipos-basicos.ts
@@ -0,0 +1,15 @@
+//variables
+
+var num1 = 1;
+let num2 = 5;
+const num3 = 4;
+
+let nombre:string='mike';
+let num:number=23;
+let activo:boolean=true;
+
+let matricula:number | string| boolean;
+matricula= 2001001;
+matricula= 'asd-123'
+matricula='false';
+
---
+++ introTscipt/src/ejemplos-ts/02--onj-interfz-arre.ts
@@ -0,0 +1,15 @@
+//objetos
+
+interface Alumno{
+matricula?:number;
+nombre:string;
+email:string
+
+}
+
+const alumno:Alumno={
+    nombre:'miguel',
+    email:'mike13@gmail.com',
+};
+
+console.log(alumno.nombre);
